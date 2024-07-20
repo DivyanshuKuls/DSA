7669387349e31a7cf249949b0a8742df99fe2e19
Divyanshu Kulshreshtha <dk42221@gmail.com>
2024-07-20 13:00:09 +0530

    twos complement

---
+++ Arrays/2s-complement.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+
+using namespace std;
+
+vector<int> findComplement(vector<int> binary) {
+    int n = binary.size();
+    vector<int> twosComp(n+1, 0);
+
+    for(int i=n-1; i>=0; i--){
+        twosComp[i] = binary[i] == 0 ? 1 : 0;
+    }
+
+    int carry = 1;
+    for(int i = n-1; i>=0; i--) {
+        int sum = twosComp[i] + carry;
+        twosComp[i] = sum % 2;
+        carry = sum / 2;
+    }
+
+    if(carry) {
+        twosComp[0] = carry;
+    }
+
+    return twosComp;
+}
+
+int main() {
+    vector<int> binary = {0, 0, 0, 0};
+    vector<int> twosComp = findComplement(binary);
+    for(int i=0; i < twosComp.size(); i++) {
+        cout << twosComp[i];
+    }
+    cout << endl;
+    return 0;
+
+}
